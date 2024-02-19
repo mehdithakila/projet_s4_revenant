d778072e100b57e3f2a6c44b3cbf1271ce28dfaf
Mehdi <mehdia45@mehdia45-VirtualBox>
2024-02-19 15:49:04 +0100

    First commit

---
+++ projet_s4/src/traitement_image/redim.rs
@@ -0,0 +1,10 @@
+use image::*;
+
+
+pub fn redim(path: &str)
+{
+    let image =open(path).unwrap();
+    image.resize(224,224,image::imageops::FilterType::Lanczos3);
+    image.save_with_format("redim_image/new",ImageFormat::Jpeg).expect("Impossible d'enregistrer l'image");
+
+}
