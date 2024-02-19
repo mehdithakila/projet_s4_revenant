2c570b1ddb05a03496a085e130c07be3fa26d804
loic.lenormand <loic.lenormand@epita.fr>
2024-02-19 15:54:03 +0100

    dossier data en dehors du cargo

--- projet_s4/src/main.rs
+++ projet_s4/src/main.rs
@@ -1,6 +1,6 @@
 mod traitement_image;
 use traitement_image::redim::redim;
 fn main() {
-    let path="data/lfw/Alicia_Witt/Alicia_Witt_0001.jpg";
+    let path="../../data/lfw/Alicia_Witt/Alicia_Witt_0001.jpg";
     redim(&path);
 }
