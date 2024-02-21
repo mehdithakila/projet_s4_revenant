a1123314e0289572860ee4522386bda867fc1446
Mehdi <mehdia45@mehdia45-VirtualBox>
2024-02-21 17:09:44 +0100

    face_detection and redim commit

---
+++ projet_s4/src/traitement_image/face_detection.rs
@@ -0,0 +1,39 @@
+use image::*;
+use super::convert_to_grey::*;
+use ndarray::*;
+use ndarray_linalg::*;
+use ndarray_linalg::error::LinalgError;
+pub fn conv_mat(images:Vec<DynamicImage>)->Array2<f64>
+{
+    let mut data:Vec<Array2<f64>> = Vec::new();
+    let (width,height)=images[0].dimensions();
+    let l=images.len();
+    for image in images
+    {
+        let grey_image=convert_to_grey(&image);
+        let flattened_img: Vec<f64> = grey_image.pixels().map(|px| px.0 as f64).collect();
+
+        let img_matrix = Array2::from_shape_vec((1, flattened_img.len()), flattened_img).unwrap();
+
+        data.push(img_matrix);     
+    }
+
+    let data_matrix = stack(Axis(0), &data.iter().map(|arr| arr.view()).collect::<Vec<_>>()).unwrap();
+    // Compute the mean vector
+    let mean_vector = data_matrix.mean_axis(Axis(0)).unwrap();
+
+    // Center the data by subtracting the mean vector
+    let centered_data = data_matrix - &mean_vector;
+
+    // Compute the covariance matrix
+    let num_samples = data_matrix.shape()[0] as f64;
+    let covariance_matrix = centered_data.into_shape((num_samples, width * height)).unwrap().t().dot(&centered_data.t()) / (num_samples - 1.0);
+    covariance_matrix
+}
+fn compute_eigen(matrix: Array2<f64>) -> Result<(Array<f64, Axis>, Array2<f64>), LinalgError> {
+    // Compute eigenvalues and eigenvectors
+    let (eigenvalues, eigenvectors) = matrix.eigh(UPLO::Upper)?;
+
+    Ok((eigenvalues, eigenvectors))
+}
+
--- projet_s4/src/traitement_image/redim.rs
+++ projet_s4/src/traitement_image/redim.rs
@@ -1,10 +1,10 @@
 use image::*;
 
 
-pub fn redim(path: &str)
+pub fn redim(path: &str)->DynamicImage
 {
     let image =open(path).unwrap();
     image.resize(224,224,image::imageops::FilterType::Lanczos3);
-    image.save_with_format("redim_image/new",ImageFormat::Jpeg).expect("Impossible d'enregistrer l'image");
-
+    //image.save_with_format("redim_image/new",ImageFormat::Jpeg).expect("Impossible d'enregistrer l'image");
+    image
 }
